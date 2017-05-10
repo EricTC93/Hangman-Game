23356ce6c6a8416cb88ea4fa79c07df3902ad800
YETC7 <tyler_726@live.com>
2017-05-10 16:25:54 -0700

    creates a phrase to display to the user

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,9 +4,26 @@ var userGuess = "";
 var guessesLeft = maxGuess;
 var maxGuess = 12;
 var guessesLeft = maxGuess;
-var wordBank = ["test","testing"];
+var phraseBank = ["test","testing"];
 
 // Generates random
-var rand = Math.floor((Math.random()*wordBank.length));
-var comWord = wordBank[rand];
-console.log(comWord);
+var rand = Math.floor((Math.random()*phraseBank.length));
+var comPhrase = phraseBank[rand];
+console.log(comPhrase);
+
+
+var displayPhrase = "";
+for (var i = 0; i < comPhrase.length; i++) {
+	displayPhrase = displayPhrase + "_";
+	console.log(comPhrase[i]);
+}
+
+if (displayPhrase.length != comPhrase.length) {
+	console.log("phrases don't match");
+}
+
+document.getElementById("wordToGuess").textContent = displayPhrase;
+
+// var test = "abcdefg";
+// test[4] = "b";
+// console.log(test.length);
