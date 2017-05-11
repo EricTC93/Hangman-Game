c20a23b56321c385a919ebf555c37c1ce24a62cf
YETC7 <tyler_726@live.com>
2017-05-10 17:33:29 -0700

    changes display text depending on letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,43 @@
 // Declaring Variables
 var alphabet = "qwertyuiopasdfghjklzxcvbnm";
-var userGuess = "";
+
+var userGuesses = [];
 var guessesLeft = maxGuess;
 var maxGuess = 12;
-var guessesLeft = maxGuess;
+
 var phraseBank = ["test","testing"];
 
-// Generates random
+// Generates random number to pick a phrase
 var rand = Math.floor((Math.random()*phraseBank.length));
 var comPhrase = phraseBank[rand];
 console.log(comPhrase);
 
-
+// Display to the user
 var displayPhrase = "";
 for (var i = 0; i < comPhrase.length; i++) {
 	displayPhrase = displayPhrase + "_";
 	console.log(comPhrase[i]);
 }
 
-if (displayPhrase.length != comPhrase.length) {
-	console.log("phrases don't match");
-}
+// if (displayPhrase.length != comPhrase.length) {
+// 	console.log("phrases don't match");
+// }
 
 document.getElementById("wordToGuess").textContent = displayPhrase;
 
-// var test = "abcdefg";
-// test[4] = "b";
-// console.log(test.length);
+
+var currentDisplayPhrase = "";
+var userLetter = "t";
+
+for (var i = 0; i < comPhrase.length; i++) {
+	if (userLetter === comPhrase[i]) {
+		currentDisplayPhrase = currentDisplayPhrase + comPhrase[i];
+	}
+
+	else {
+		currentDisplayPhrase = currentDisplayPhrase + "_";
+	}
+}
+
+console.log(currentDisplayPhrase);
+document.getElementById("wordToGuess").textContent = currentDisplayPhrase;
